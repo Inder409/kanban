a8bbd87c36b790a61d2014f9d4602428c4a4a3bb
MSI\inder <inderpreet409@gmail.com>
2022-08-29 22:29:03 -0400

    Tested out in postman

--- Apiassigment.php
+++ Apiassigment.php
@@ -3,3 +3,5 @@ header('Access-Control-Allow-Origin:*');
 header('Content-Type:application/json');
 header('Access-Control-Allow-Methods:POST');
 header('Access-Control-Allow-Headers:Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+$data=file_get_contents("php://input");
+each $data;
